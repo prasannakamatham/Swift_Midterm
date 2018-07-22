d1624dedae4b4420d776e2ad6b95746eb55fad1c
Nikhil Kothamasu <nikhilkothamasu@nikhils-macbook-pro.local>
2018-07-21 22:44:27 -0400

    Initial Commit

---
+++ CO737608_Midterm_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CO737608_Midterm_MAD3004
+//
+//  Created by Nikhil Kothamasu on 2018-07-21.
+//  Copyright © 2018 prasanna kamatham. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
